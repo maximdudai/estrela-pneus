a1add9d1dff5054617d9d6a60211a61ae12ad6f5
maximdudai <maxim.dudai01@gmail.com>
2023-04-09 23:08:56 +0100

    add some responsive screens for full responsive words using grid display

--- src/components/data/SearchData.jsx
+++ src/components/data/SearchData.jsx
@@ -4,14 +4,14 @@ const SearchData = ({ clickedList, searchFilter }) => {
     return (
       <ul 
         // className="w-full flex flex-wrap"
-        className="grid grid-cols-2 gap-4 md:grid-cols-4"
+        className="grid overflow-y-hidden grid-cols-2 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4"
         >
         {filteredWords.map((word, i) => (
           <li
             onClick={clickedList}
             key={i}
             // className="w-auto text-center m-1 p-1 px-2 text-gray-600 rounded border-[1px] border-gray-400"
-            className="bg-gray-200 rounded-md p-4 text-center"
+            className="bg-gray-200 rounded-md p-4 text-center whitespace-normal"
           >
             {word}
           </li>
