fe1186a1b80943bb9b571f5af7833a1ff9c44c7e
maximdudai <maxim.dudai01@gmail.com>
2023-04-09 23:03:17 +0100

    displaying suggestions using grid from flex

--- src/components/data/SearchData.jsx
+++ src/components/data/SearchData.jsx
@@ -2,12 +2,16 @@ const SearchData = ({ clickedList, searchFilter }) => {
     const filteredWords = searchFilter();
   
     return (
-      <ul className="w-full flex flex-wrap">
+      <ul 
+        // className="w-full flex flex-wrap"
+        className="grid grid-cols-2 gap-4 md:grid-cols-4"
+        >
         {filteredWords.map((word, i) => (
           <li
             onClick={clickedList}
             key={i}
-            className="w-auto m-1 p-1 px-2 text-gray-600 rounded border-[1px] border-gray-400"
+            // className="w-auto text-center m-1 p-1 px-2 text-gray-600 rounded border-[1px] border-gray-400"
+            className="bg-gray-200 rounded-md p-4 text-center"
           >
             {word}
           </li>
