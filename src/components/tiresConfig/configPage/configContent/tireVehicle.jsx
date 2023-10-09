8d394d586fd1c2cc2ebe51ffcf86fdea1530fab3
maximdudai <maxim.dudai01@gmail.com>
2023-10-09 21:34:44 +0100

    initiliazing tire config by vehicle component

--- src/components/tiresConfig/configPage/configContent/tireVehicle.jsx
+++ src/components/tiresConfig/configPage/configContent/tireVehicle.jsx
@@ -1,10 +1,31 @@
-import * as carData from '../../../../assets/cars/car-models.json';
+import { useState } from "react";
+import carData from "../../../../assets/cars/car-models.json";
 
 const TireVehicleModel = () => {
-    return (
-        <>
-    
-        </>
-    )
+  const [selecteedVehicle, setSelecteedVehicle] = useState("");
+  const [selectedModel, setSelectedModel] = useState("");
+
+  const handleSelectVehicle = (item) => {
+    const vehicle = item.target.value;
+
+    setSelecteedVehicle(vehicle);
+  };
+
+  return (
+    <>
+      <div className="tireVehicleConfig w-full flex flex-col items-center mt-10">
+        <p className="p-2 text-gray-400">Selecione o seu modelo </p>
+        <select onClick={handleSelectVehicle} className="w-1/2 text-3xl border-2 p-2">
+          {carData.map((car, index) => {
+            return (
+              <option value={car.brand} key={index}>
+                {car.brand}
+              </option>
+            );
+          })}
+        </select>
+      </div>
+    </>
+  );
 };
 export default TireVehicleModel;
--- src/pages/ErrorBoundry/ErrorBoundry.jsx
+++ src/pages/ErrorBoundry/ErrorBoundry.jsx
@@ -9,7 +9,7 @@ export default function ErrorPage() {
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+      <p style={{color: 'white'}}>
         <i>{error?.statusText || error?.message}</i>
       </p>
     </div>
